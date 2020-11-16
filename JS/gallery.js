bfe024da1ec31e0e4924a07f290cc246133a1ab4
Maksym Zyza <mmziza88@gmail.com>
2020-11-16 21:37:47 +0200

    Закрытие модалки по ESC

--- JS/gallery.js
+++ JS/gallery.js
@@ -8,32 +8,30 @@ const refs = {
 
 function getImgParam({ preview, description }) {
   return `<li class="gallery__item">
-  <img class="gallery__image" src=${preview} alt=${description} 
+  <img class="gallery__image" src=${preview} alt='${description}'> 
     </li>`;
 }
 
 function getImg(arr) {
-  const list = `${arr.map((item) => getImgParam(item)).join(" ")}`;
+  const list = `${arr.map((item) => getImgParam(item)).join("")}`;
   return list;
 }
 
 refs.galleryList.insertAdjacentHTML("beforeend", getImg(gallery));
 console.log(refs.galleryList);
 
-// =====================================
 // 2.Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
 refs.galleryList.addEventListener("click", onImgClick);
 
 function onImgClick(event) {
   if (event.target.nodeName !== "IMG") {
-    console.log("Кликнули не по картинке, ничего не делаем");
+    console.log("Кликнули не по картинке.");
     return;
   }
 
   const nextActiveImg = event.target;
 
   setActiveImg(nextActiveImg);
-  //   getImgUrl(nextActiveImg);
   openModal(nextActiveImg);
 }
 
@@ -41,27 +39,21 @@ function setActiveImg(nextActiveImg) {
   const currentActiveImg = refs.galleryList.querySelector(".js-img--active");
 
   if (currentActiveImg) {
-    // console.log('Уже есть активный, снимаю класс!');
     currentActiveImg.classList.remove("js-img--active");
   }
 
   nextActiveImg.classList.add("js-img--active");
 }
 
-// const getImgUrl = (img) =>
-//   gallery.find(({ preview, original }) => {
-//     if (preview === img.src) {
-//       console.log("original - ", original);
-//     }
-//   });
-
 // 3.Открытие модального окна по клику на элементе галереи.
 const modalDiv = document.querySelector(".js-lightbox");
 console.log(modalDiv);
 const butonClose = modalDiv.querySelector('[data-action="close-lightbox"]');
 const openImg = modalDiv.querySelector(".lightbox__image");
+const overlayDiv = modalDiv.querySelector(".lightbox__overlay");
 
 function openModal(img) {
+  window.addEventListener("keydown", onPressEscape);
   modalDiv.classList.add("is-open");
 
   // 4.Подмена значения атрибута src элемента img.lightbox__image.
@@ -73,10 +65,20 @@ function openModal(img) {
 }
 
 // 5.Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+// +Закрытие модального окна по клику на div.lightbox__overlay
+// +Закрытие модального окна по нажатию клавиши ESC
 butonClose.addEventListener("click", closeModal);
+overlayDiv.addEventListener("click", closeModal);
 
 function closeModal() {
+  window.removeEventListener("keydown", onPressEscape);
   modalDiv.classList.remove("is-open");
   // 6.Очистка значения атрибута src элемента img.lightbox__image.
   openImg.src = "";
+}
+
+function onPressEscape(event) {
+  if (event.code === "Escape") {
+    closeModal();
+  }
 }
